1d96b8ab0ee16146b6b08329e816a8da71889285
Verta-master <vertyagina@gmail.com>
2018-03-13 20:46:45 +0600

    Content

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.detail-slider').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 3000
+    });
+});
